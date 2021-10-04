7ddfa50131e15a10e41ef58d329dbecabe629e76
Emily <89871431+emillly-b@users.noreply.github.com>
2021-10-03 22:19:39 -0500

    Create GuildHandlingService.cs

---
+++ SaturnBot/SaturnBot/Services/GuildHandlingService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace SaturnBot.Services
+{
+    public class GuildHandlingService
+    {
+        private readonly CommandService _commands;
+        private readonly DiscordShardedClient _discord;
+        private readonly IServiceProvider _services;
+
+        public GuildHandlingService(IServiceProvider services)
+        {
+            _commands = services.GetRequiredService<CommandService>();
+            _discord = services.GetRequiredService<DiscordShardedClient>();
+            _services = services;
+
+            _discord.GuildAvailable += GuildAvailable;
+            
+        }
+
+        private Task GuildAvailable(SocketGuild arg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+    }
+}
