f3a6b971ba6b77f0531b3762396693f266916e61
Emily <89871431+emillly-b@users.noreply.github.com>
2021-10-04 21:28:14 -0500

    Added fun

--- SaturnBot/SaturnBot/Modules/PublicModule.cs
+++ SaturnBot/SaturnBot/Modules/PublicModule.cs
@@ -1,4 +1,6 @@
 using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
 using Microsoft.Extensions.DependencyInjection;
 using Discord.Commands;
 using Discord;
@@ -30,6 +32,27 @@ namespace SaturnBot.Modules
             var author = Context.Message.Author.Mention;
             var mention = Context.Guild.GetUser(id).Mention;
             await ReplyAsync($"{author} has chosen {mention} as their lord and savior, praise {mention}");
+        }
+
+        [Command("quiz")]
+        public async Task ShitListAsync()
+        {
+            var chars = new string[,]
+            {
+                {"Max", "https://decider.com/wp-content/uploads/2019/06/stranger-things-max.jpg?quality=90&strip=all&w=646&h=431&crop=1"},
+                { "Dustin", "https://static.wikia.nocookie.net/strangerthings8338/images/1/18/Dustin_S2.png/revision/latest?cb=20180319174421"},
+                { "Eleven",  "https://media.vanityfair.com/photos/59f370dd16ff751cf425ef46/master/pass/wv_publicity_pre_launch_A_still_23.jpg"},
+                { "Billy", "https://adultpaintbynumber.com/wp-content/uploads/2020/10/Billy-Hargrove-paint-by-numbers.jpg" },
+                { "Billy", "https://www.pinclipart.com/picdir/middle/91-914392_billy-png-clipart-billy-y-mandy-render-transparent.png" }
+            };
+            var rand = new Random();
+            var index = rand.Next(4);
+            var builder = new EmbedBuilder()
+                .WithTitle($"You are {chars[index, 0]}!")
+                .WithImageUrl($"{chars[index, 1]}")
+                .WithFooter("List Credit: WickedCat!");
+            var embed = builder.Build();
+            await Context.Message.ReplyAsync("", embed: embed);
         }
     }
 }
