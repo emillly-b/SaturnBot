a5c7d737d040c51b927bc7efcf001ed7b9676636
Emily <89871431+emillly-b@users.noreply.github.com>
2021-10-04 22:55:16 -0500

    Begin Database Building

---
+++ SaturnBot/SaturnBot/Entities/Guild.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaturnBot.Entities
+{
+    public class Guild
+    {        
+        public ulong Id { get; set; }
+        public string Prefix { get; set; }
+        public List<ulong> MutedUsers { get; set; }
+        public ulong LoggingChannelId { get; set; }
+        public ulong VerifiedRole { get; set; }
+        public List<User> Members { get; set; }
+
+        public Guild()
+        {
+            //
+        }
+    }
+}
---
+++ SaturnBot/SaturnBot/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaturnBot.Entities
+{
+    public class User
+    {
+        public ulong Id { get; set; }
+        public List<string> Usernames { get; set; }
+        public List<string> Nicknames { get; set; }
+        public List<string> Warnings { get; set; }
+        public ulong Messages { get; set; }
+        public bool IsPresent { get; set; }
+    }
+}
