406d6f2842e852091db1b518158043716b1a602a
Emily <89871431+emillly-b@users.noreply.github.com>
2021-10-11 17:10:08 -0500

    Forgot this file.

--- SaturnBot/SaturnBot/Services/ReactionHandlingService.cs
+++ SaturnBot/SaturnBot/Services/ReactionHandlingService.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Reflection;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using SaturnBot.Entities;
 
 namespace SaturnBot.Services
 {
@@ -12,6 +14,8 @@ namespace SaturnBot.Services
     {
         private DiscordShardedClient _discord;
         private IServiceProvider _services;
+        public List<Starboard> ActiveStarboards;
+
         public ReactionHandlingService(IServiceProvider services)
         {
             _services = services;
@@ -19,12 +23,25 @@ namespace SaturnBot.Services
         public void Initialize()
         {
             var client = _services.GetRequiredService<DiscordShardedClient>();
+            var guilds = _services.GetRequiredService<GuildHandlingService>();
+            foreach(Guild guild in guilds.ActiveGuilds)
+            {
+                try
+                {
+                    if (guild.Starboard.IsEnabled)
+                        ActiveStarboards.Add(guild.Starboard);
+                }
+                catch
+                {
+                    guild.Starboard = new Starboard();
+                }
+            }
             client.ReactionAdded += ReactionAdded;
         }
         public async Task ReactionAdded(Cacheable<IUserMessage, ulong> cachedMessage, Cacheable<IMessageChannel, ulong> messageChannel, SocketReaction reaction)
         {
-            if (reaction.Emote.Name.Equals("⭐"))
-                Console.WriteLine("Star Recieved!");
+            //if (reaction.Emote.Name.Equals("⭐"))
+            //    Console.WriteLine("Star Recieved!");
         }
     }
 }
